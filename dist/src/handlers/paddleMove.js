4bee81b8f7a05c3125e7840561c1b18600ce2039
Ben Kile <benjaminfkile@gmail.com>
2025-09-07 16:11:56 -0500

    Stop tracking dist folder

--- dist/src/handlers/paddleMove.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function handlePaddleMove(socket) {
-    socket.on("paddleMove", (data) => {
-        console.log("📤 paddleMove:", data);
-        socket.broadcast.emit("paddleMove", data);
-    });
-}
-exports.default = handlePaddleMove;
